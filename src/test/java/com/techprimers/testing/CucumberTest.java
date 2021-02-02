bf4d815cf6c674f3733d4cbfa8fafd558f46b675
Lukasz Chomatek <lukasz.chomatek@p.lodz.pl>
2021-02-02 19:55:09 +0100

    added polish scenario

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         glue = "com.techprimers.testing",
-        format = {"json:target/cucumber.json"}
+        //format = {"json:target/cucumber.json"}
+        format = {"pretty", "html:target/Destination"}
 )
 public class CucumberTest {
 }
