e333db36226e9c2ce54b1cac8db857645180c9b9
Lukasz Chomatek <lukasz.chomatek@p.lodz.pl>
2021-02-02 19:59:56 +0100

    corrected cucumber output format

--- src/test/java/com/techprimers/testing/CucumberTest.java
+++ src/test/java/com/techprimers/testing/CucumberTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources",
         glue = "com.techprimers.testing",
-        //format = {"json:target/cucumber.json"}
-        format = {"pretty", "html:target/Destination"}
+        format = {"json:target/cucumber.json"}
+        //format = {"pretty", "html:target/Destination"}
 )
 public class CucumberTest {
 }
